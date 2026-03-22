64d2a09df707cdfdd49d36d233520e8c90d85fd3
Yaswini Priya Ganji <yaswiniganji@outlook.com>
2026-03-22 23:43:55 +0530

    Initial commit - cleaned temp files

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FlipsoApp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
